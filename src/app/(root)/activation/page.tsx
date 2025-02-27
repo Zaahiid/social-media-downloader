99d1f48bf2696182b05be7a89db684600dd42503
Zaahiid <zahidrsheikh01@gmail.com>
2025-02-27 22:29:07 +0530

    Added Activation page

---
+++ src/app/(root)/activation/page.tsx
@@ -0,0 +1,23 @@
+import ActivationForm from "@/components/globle/activation-form";
+import { getProductInfo } from "@/lib/fetch";
+import { redirect } from "next/navigation";
+
+const ActivationPage = async () => {
+  let res;
+
+  try {
+    res = await getProductInfo();
+  } catch (error) {
+    res = "error";
+  }
+
+  const productInfo = res["product-info"];
+
+  if (productInfo) {
+    redirect("/");
+  }
+
+  return <ActivationForm />;
+};
+
+export default ActivationPage;
