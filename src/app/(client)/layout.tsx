cf7a424361e7f8f95a35f9b0c6c8394141fc6ee9
Zaahiid <zahidrsheikh01@gmail.com>
2025-02-28 07:35:50 +0530

    Added Client Layout

---
+++ src/app/(client)/layout.tsx
@@ -0,0 +1,14 @@
+
+export default async function RootLayout({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+    return (
+      <>
+        <main className="overflow-hidden">
+          <div className="w-screen min-h-screen container">{children}</div>
+        </main>
+      </>
+    );
+  }
